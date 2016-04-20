982c82e8f07e022bb84a45cf325115f0417eedc9
Shane Starcher <shanestarcher@gmail.com>
2016-04-20 13:37:33 -0400

    now outputs to a file and logs debug to stdout

--- main.go
+++ main.go
@@ -1,59 +1,94 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
 	"strings"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/ec2metadata"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/iam"
 )
 
-func shellEncode(key, value string) {
+func shellEncode(key, value string, output io.Writer) {
 	key = strings.ToUpper(key)
 	key = strings.Replace(key, ":", "_", -1)
 	key = strings.Replace(key, "-", "_", -1)
-	fmt.Printf("AWS_%s=%s\n", strings.Replace(strings.ToUpper(key), ":", "_", -1), value)
+	fmt.Fprintf(output, "AWS_%s=%s\n", strings.Replace(strings.ToUpper(key), ":", "_", -1), value)
+}
+
+func init() {
+	log.SetOutput(os.Stderr)
 }
 
 func main() {
 	session := session.New()
 	metadata := ec2metadata.New(session)
-
-	instanceID, err := metadata.GetMetadata("instance-id")
+	f, err := os.Create("/etc/aws")
 	if err != nil {
-		panic(err)
+		log.Panicf("Failed to create file /etc/aws, %v", err)
 	}
-	shellEncode("INSTANCE_ID", instanceID)
+	defer f.Close()
+	writer := bufio.NewWriter(f)
 
-	region, err := metadata.Region()
+	var instanceID string
+	if metadata.Available() {
+		instanceID, err = metadata.GetMetadata("instance-id")
+		if err != nil {
+			log.Panicf("Failed to fetch instance-id, %v", err)
+		}
+		shellEncode("INSTANCE_ID", instanceID, writer)
+
+		az, err := metadata.GetMetadata("placement/availability-zone")
+		if err != nil {
+			log.Panicf("Failed to fetch availablity-zone, %v", err)
+		}
+		shellEncode("AVAILABLITY_ZONE", az, writer)
+
+		region, err := metadata.Region()
+		if err != nil {
+			log.Panicf("Failed to fetch Region, %v", err)
+		}
+		shellEncode("REGION", region, writer)
+		session = session.Copy(&aws.Config{Region: aws.String(region)})
+	} else {
+		instanceID = os.Args[1]
+	}
+
+	iamClient := iam.New(session)
+	aliases, err := iamClient.ListAccountAliases(&iam.ListAccountAliasesInput{})
 	if err != nil {
-		panic(err)
+		log.Panicf("Failed to ListAccountAliases, %v", err)
 	}
-	shellEncode("REGION", region)
 
-	az, err := metadata.GetMetadata("placement/availability-zone")
-	if err != nil {
-		panic(err)
+	for _, alias := range aliases.AccountAliases {
+		shellEncode("ACCOUNT_ALIAS", *alias, writer)
 	}
-	shellEncode("AVAILABLITY_ZONE", az)
 
-	ec2client := ec2.New(session, aws.NewConfig().WithRegion(region))
-	resp, err := ec2client.DescribeInstances(&ec2.DescribeInstancesInput{
+	log.Info("Describing instances")
+	ec2Client := ec2.New(session)
+	resp, err := ec2Client.DescribeInstances(&ec2.DescribeInstancesInput{
 		InstanceIds: []*string{
 			aws.String(instanceID),
 		},
 	})
 	if err != nil {
-		panic(err)
+		log.Panicf("Failed to DescribeInstances, %v", err)
 	}
 
 	for _, res := range resp.Reservations {
 		for _, inst := range res.Instances {
+			log.Infof("Instance ID: %s", *inst.InstanceId)
 			for _, tag := range inst.Tags {
-				shellEncode("Tag_"+*tag.Key, *tag.Value)
+				shellEncode("Tag_"+*tag.Key, *tag.Value, writer)
 			}
 		}
 	}
+
+	writer.Flush()
 }
